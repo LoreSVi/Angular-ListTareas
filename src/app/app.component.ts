93906d54a0ab07b77e04cda89a107755e64b3473
LoreSVi <loresuvivas@gmail.com>
2024-08-01 17:26:44 +0200

    lista tareas angular 17

---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+/*import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'listaTareasAngular';
+}
+*/
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TareasService } from './services/tareas.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  
+  listaTareas:string[] = [];
+  nuevaTarea:string = '';
+  
+  private _tareasService = inject(TareasService);
+  
+  
+  ngOnInit(): void {
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  agregarTarea(){
+    this._tareasService.agregarTarea(this.nuevaTarea);
+    this.nuevaTarea = '';
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  eliminarTarea(index: number){
+    this._tareasService.eliminarTarea(index);
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+}
