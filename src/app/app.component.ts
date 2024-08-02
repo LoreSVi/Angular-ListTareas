a60a3ebeb7b0cd8bca7e9a4db94e0e446f83621b
LoreSVi <loresuvivas@gmail.com>
2024-08-02 08:11:04 +0200

    retoque estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,3 @@
-/*import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-  title = 'listaTareasAngular';
-}
-*/
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
